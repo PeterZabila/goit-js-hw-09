f456d045db11ab2f6c981101e9f3162ceac132de
Peter Zabila <petrozabila@gmail.com>
2022-06-02 00:39:41 +0300

    2nd task in progress

---
+++ src/js/02-timer.js
@@ -0,0 +1,28 @@
+import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
+
+const refs =  {
+    input: document.querySelector("input#datetime-picker"),
+    startBtn: document.querySelector("button[data-start]"),
+};
+
+flatpickr("input#datetime-picker", {enableTime: true,
+    dateFormat: "Y-m-d H:i",});
+
+
+    // refs.startBtn.addEventListener('click', startBtnHandler);
+    refs.input.addEventListener('input', inputHandler)
+
+function inputHandler(e) {
+    
+    const inputValue = e.target.value;
+  
+    if(inputValue) {
+        console.log(inputValue);
+        const time = new Date(inputValue);
+        console.log(time.getTime());
+        console.log(time);
+    }
+    
+};
